89a9de81195c2fda71713d4da6027ddae0173e40
jh178bz <jh178bz@gmail.com>
2020-07-26 14:27:40 +0900

    各viewの調整とuser個別ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
+
   root 'pages#home'
-  get 'pages/home'
-  get 'pages/terms'
-  get 'pages/about'
-  get 'pages/contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/home' ,   to: 'pages#home'
+  get '/terms',   to: 'pages#terms'
+  get '/about',   to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+
+  resources :users, only:[:show]
 end
