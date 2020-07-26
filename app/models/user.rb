6e8993b533c525acec6f2b012dd38ac15bcf4dc7
jh178bz <jh178bz@gmail.com>
2020-07-26 21:08:05 +0900

    follow機能db周りの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,:omniauthable,omniauth_providers:[:facebook]
 
@@ -21,4 +29,19 @@ class User < ApplicationRecord
      super
    end
  end
+
+ # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
